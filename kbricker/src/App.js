e07d1c614da8c05b09d8ff7c9889d941224803e8
Kyle <kbricker@ualberta.ca>
2025-01-07 12:23:07 -0700

    Added section for languages/technologies and some transitions on hover

--- kbricker/src/App.js
+++ kbricker/src/App.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 
 import MainCard from './components/mainCard';
 import ItemGrid from './components/itemGrid';
--- kbricker/src/components/itemCard.js
+++ kbricker/src/components/itemCard.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 
 const images = require.context('../img/projects', false, /\.(png|jpe?g|webp)$/);
 
@@ -13,8 +13,16 @@ function ItemCard(props) {
             <div className='gridImageWrap'>
                 <img src={image} alt={itemData.title} />
             </div>
-            <div className='gridContentWrap'>
-                <h1>{itemData.title}</h1>
+            <div className='gridContentWrap scrim'>
+                <div className='textWrap'>
+                    <div className='itemCardTitle'>
+                        <h1>{itemData.title}</h1>
+                    </div>
+                    <div className='itemCardDetails'>
+                        <hr class="solid"/>
+                        <p>HTML • CSS</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
